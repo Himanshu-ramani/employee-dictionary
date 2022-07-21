b38dd32d7343c40f038ff5062efd25b9b9879b02
Himanshu-Ramani <ramanihimanshu58@gmail.com>
2022-07-21 17:29:30 +0530

    write a test case with avoid redux

--- src/Component/AuthPage/AuthPage.test.js
+++ src/Component/AuthPage/AuthPage.test.js
@@ -3,13 +3,15 @@ import AuthPage from "./AuthPage";
 import { Provider } from "react-redux";
 import store from "../../ReduxStore/index";
 import { Route, Routes } from "react-router-dom";
-
+import LoginPage from "../../Pages/LoginPage";
+import { createMemoryHistory } from "history";
 describe("Auth page comoponent", () => {
   test("first name Should be rendered", () => {
+    const history = createMemoryHistory();
     render(
       <Provider store={store}>
-        <Routes>
-          <Route element={<AuthPage />} />
+        <Routes history={history}>
+          <LoginPage />
         </Routes>
       </Provider>
     );
